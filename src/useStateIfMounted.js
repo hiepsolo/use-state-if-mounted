f5c7290e7f8573d0a5ee931d27cf4f2ea517a035
Hiep Nguyen <hoanghiep2004hy@gmail.com>
2021-04-30 15:08:07 +0700

    Add source code and read me

---
+++ src/useStateIfMounted.js
@@ -0,0 +1,26 @@
+import useIsComponentMounted from './useIsComponentMounted';
+import { useState, useCallback } from 'react';
+
+/**
+ * Like React's [useState](https://reactjs.org/docs/hooks-reference.html#usestate)
+ * but it makes sure the component that uses this hook is mounted when updating state
+ *
+ * @see https://reactjs.org/docs/hooks-reference.html#usestate
+ * @export
+ * @param {any} initialValue
+ * @returns {[any, Diapatch<any>]} an array of 2 items
+ * the first is the current state, the second is a function that enables
+ * updating the state if the component is not mounted
+ */
+export default function useStateIfMounted(initialValue) {
+	const isComponentMounted = useIsComponentMounted();
+	const [state, setState] = useState(initialValue);
+
+	const newSetState = useCallback((value) => {
+		if (isComponentMounted.current) {
+			setState(value);
+		}
+	});
+
+	return [state, newSetState];
+}
